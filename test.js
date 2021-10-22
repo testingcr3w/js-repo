ecb86b1aa3524805cb4bce393f8570f10af2292b
testingcr3w <92932374+testingcr3w@users.noreply.github.com>
2021-10-22 12:00:27 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 var test ='test-message';
 
-function test_function(test) {
-	
-	alert(test);
+function test_function(cmd) {
+	var string = cmd
+	alert(cmd);
+	alert(document.cookie);
 	console.log('it works');
 	
 }
